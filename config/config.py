d184d1bbb2fd553e590006db2b7826779316f54f
syehan269 <syehan269@gmail.com>
2022-01-09 15:17:10 +0700

    add more information

--- config/config.py
+++ config/config.py
@@ -1,12 +1,18 @@
 class Config:
     def __init__(self) -> None:
-        self.app_name = 'Prototype'
+
+        self.app_name = 'PNote'
+        self.author = 'Dayyan Syehan Al Akbar'
+        self.info = '1A - S1 Sistem Informasi'
+        self.repository_link = 'https://github.com/syehan269/UAS-DPK'
+        self.nim = '21083000016'
+
         self.window_size = '350x500'
 
         self.main_margin = 10
         self.secondary_margin = 15
         self.small_margin = 5
-        
+
         self.primary_color = '#3D813F'
         self.secondary_color = '#E5E5E5'
         self.text_color = '#000'
